bc57ba1904dee452d1e34e96d032849f3dd7732b
geraldo <geraldo@servus.at>
2020-12-07 11:39:56 +0100

    use live qgis server icon for layer if mapproxy=false

--- js/libs/ol-layerswitcher.js
+++ js/libs/ol-layerswitcher.js
@@ -371,7 +371,7 @@ var LayerSwitcher = function (_Control) {
                     if (lyr.get('title') === 'Catastro') {
                         img.src = 'http://ovc.catastro.meh.es/Cartografia/WMS/simbolos.png?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetLegendGraphic&LAYER=Catastro&FORMAT=image/png&SLD_VERSION=1.1.0';
                     } 
-                    else if (!lyr.mapproxy) {
+                    else if (!lyr.get('mapproxy') && lyr.get('mapproxy') !== undefined) {
                         // dynamic from qgis server
                         img.src = map.get("urlWMSqgis") + '?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetLegendGraphic&LAYER='+lyrTitle+'&FORMAT=image/png&SLD_VERSION=1.1.0';
                     }
