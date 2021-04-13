b2bf23405bd1dbfe6d376f8d7c2166356d9661ff
geraldo <geraldo@servus.at>
2021-04-13 16:25:05 +0200

    quitar icon

--- tpl/default/tpl_home.php
+++ tpl/default/tpl_home.php
@@ -4,11 +4,6 @@
 		<title><?php echo $title; ?> de Castellbisbal</title>
 
         <link rel="shortcut icon" href="http://www.castellbisbal.cat/themes/castellbisbal/images/favicons/favicon.ico"> 
-        <link rel="apple-touch-icon" sizes="57x57" href="http://www.castellbisbal.cat/front/view/images/favicons/apple-touch-icon-57x57.png"> 
-        <link rel="apple-touch-icon" sizes="114x114" href="http://www.castellbisbal.cat/front/view/images/favicons/apple-touch-icon-114x114.png"> 
-        <link rel="apple-touch-icon" sizes="72x72" href="http://www.castellbisbal.cat/front/view/images/favicons/apple-touch-icon-72x72.png"> 
-        <link rel="apple-touch-icon" sizes="60x60" href="http://www.castellbisbal.cat/front/view/images/favicons/apple-touch-icon-60x60.png"> 
-        <link rel="apple-touch-icon" sizes="76x76" href="http://www.castellbisbal.cat/front/view/images/favicons/apple-touch-icon-76x76.png"> 
         <link rel="icon" type="image/png" href="http://www.castellbisbal.cat/front/view/images/favicons/favicon-96x96.png" sizes="96x96"> 
         <link rel="icon" type="image/png" href="http://www.castellbisbal.cat/themes/castellbisbal/images/favicons/favicon-16x16.png" sizes="16x16">
         <link rel="icon" type="image/png" href="http://www.castellbisbal.cat/themes/castellbisbal/images/favicons/favicon-32x32.png" sizes="32x32"><meta name="msapplication-TileColor" content="#cee1ef"> 
